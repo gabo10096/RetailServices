dd17a2b958205e5407085da104c0c22cb2523e8c
Gabriel Aguilar Gonzalez <gabo10096@hotmail.com>
2019-08-17 15:15:18 -0500

    se integra oauthservice con docker

--- api-manager/src/main/java/mx/com/project/zuul/filters/SimpleFilter.java
+++ api-manager/src/main/java/mx/com/project/zuul/filters/SimpleFilter.java
@@ -2,12 +2,20 @@ package mx.com.project.zuul.filters;
 
 import com.netflix.zuul.ZuulFilter;
 import com.netflix.zuul.context.RequestContext;
+import mx.com.project.zuul.services.ITokenService;
+import mx.com.project.zuul.to.ValidateTokenRequestTO;
 import org.apache.log4j.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+
 import javax.servlet.http.HttpServletRequest;
 
 public class SimpleFilter extends ZuulFilter {
 
     final static Logger log = Logger.getLogger(SimpleFilter.class);
+
+    @Autowired
+    ITokenService tokenService;
 
     @Override
     public String filterType() {
@@ -31,6 +39,18 @@ public class SimpleFilter extends ZuulFilter {
 
         log.info(String.format("%s request to %s", request.getMethod(), request.getRequestURL().toString()));
 
+        ValidateTokenRequestTO validateTokenRequest = new ValidateTokenRequestTO();
+
+        validateTokenRequest.setToken(request.getHeader("Authorization").replace("Bearer ", ""));
+        validateTokenRequest.setUser(request.getHeader("User"));
+
+        try {
+            tokenService.validateToken(validateTokenRequest);
+        }catch (Exception e){
+            ctx.setSendZuulResponse(false);
+            ctx.setResponseBody("Peticion invalida");
+            ctx.setResponseStatusCode(HttpStatus.UNAUTHORIZED.value());
+        }
         return null;
     }
 }
